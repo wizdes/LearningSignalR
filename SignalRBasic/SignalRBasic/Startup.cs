05b199f3695787d6c785e65f70bcbf8a8900916e
Yi Li <yjlics@gmail.com>
2014-11-17 18:13:16 -0800

    AddedFirstProject

---
+++ SignalRBasic/SignalRBasic/Startup.cs
@@ -0,0 +1,16 @@
+﻿using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SignalRBasic
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR<TrackerConnection>("/tracker");
+        }
+    }
+}
---
+++ SignalRBasic/SignalRBasic/TrackerConnection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+
+namespace SignalRBasic
+{
+    public class TrackerConnection : PersistentConnection
+    {
+        protected override Task OnReceived(IRequest request, string connectionId, string data)
+        {
+            return Connection.Broadcast(data);
+        }
+    }
+}

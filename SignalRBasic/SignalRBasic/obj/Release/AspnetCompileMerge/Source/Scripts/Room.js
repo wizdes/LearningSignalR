932a778cba2d829bef3dde9d9b4d04a05aa591f1
Yi Li <yl2326@cornell.edu>
2014-12-03 11:36:19 -0800

    fixed some javascript.

--- SignalRBasic/SignalRBasic/obj/Release/AspnetCompileMerge/Source/Scripts/Room.js
+++ SignalRBasic/SignalRBasic/obj/Release/AspnetCompileMerge/Source/Scripts/Room.js
@@ -85,133 +85,134 @@ $(function() {
         var value2 = $("#chatName").val();
         hub.server.leaveRoom(value2);
     });
-
-
-    function chatWith(chatuser) {
-        openedConversations.push(chatuser);
-        createChatBox(chatuser);
-        $("#chatbox_" + chatuser + " .chatboxtextarea").focus();
-    }
-
-    function createChatBox(chatboxtitle, minimizeChatBox) {
-        if ($("#chatbox_" + chatboxtitle).length > 0) {
-            if ($("#chatbox_" + chatboxtitle).css('display') == 'none') {
-                $("#chatbox_" + chatboxtitle).css('display', 'block');
-                restructureChatBoxes();
-            }
-            $("#chatbox_" + chatboxtitle + " .chatboxtextarea").focus();
-            return;
-        }
-
-        $(" <div />").attr("id", "chatbox_" + chatboxtitle)
-            .addClass("chatbox")
-            .html('<div class="chatboxhead"><div class="chatboxtitle">' + chatboxtitle + '</div><div class="chatboxoptions"><a href="javascript:void(0)" onclick="javascript:toggleChatBoxGrowth(\'' + chatboxtitle + '\')">-</a> <a href="javascript:void(0)" onclick="javascript:closeChatBox(\'' + chatboxtitle + '\')">X</a></div><br clear="all"/></div><div class="chatboxcontent"></div><div class="chatboxinput"><textarea class="chatboxtextarea" onkeydown="javascript:return checkChatBoxInputKey(event,this,\'' + chatboxtitle + '\');"></textarea></div>')
-            .appendTo($("body"));
-
-        $("#chatbox_" + chatboxtitle).css('bottom', '0px');
-
-        chatBoxeslength = 0;
-
-        for (x in chatBoxes) {
-            if ($("#chatbox_" + chatBoxes[x]).css('display') != 'none') {
-                chatBoxeslength++;
-            }
-        }
-
-        if (chatBoxeslength == 0) {
-            $("#chatbox_" + chatboxtitle).css('right', '20px');
-        } else {
-            width = (chatBoxeslength) * (225 + 7) + 20;
-            $("#chatbox_" + chatboxtitle).css('right', width + 'px');
-        }
-
-        chatBoxes.push(chatboxtitle);
-
-        if (minimizeChatBox == 1) {
-            minimizedChatBoxes = new Array();
-
-            if ($.cookie('chatbox_minimized')) {
-                minimizedChatBoxes = $.cookie('chatbox_minimized').split(/\|/);
-            }
-            minimize = 0;
-            for (j = 0; j < minimizedChatBoxes.length; j++) {
-                if (minimizedChatBoxes[j] == chatboxtitle) {
-                    minimize = 1;
-                }
-            }
-
-            if (minimize == 1) {
-                $('#chatbox_' + chatboxtitle + ' .chatboxcontent').css('display', 'none');
-                $('#chatbox_' + chatboxtitle + ' .chatboxinput').css('display', 'none');
-            }
-        }
-
-        chatboxFocus[chatboxtitle] = false;
-
-        $("#chatbox_" + chatboxtitle + " .chatboxtextarea").blur(function() {
-            chatboxFocus[chatboxtitle] = false;
-            $("#chatbox_" + chatboxtitle + " .chatboxtextarea").removeClass('chatboxtextareaselected');
-        }).focus(function() {
-            chatboxFocus[chatboxtitle] = true;
-            newMessages[chatboxtitle] = false;
-            $('#chatbox_' + chatboxtitle + ' .chatboxhead').removeClass('chatboxblink');
-            $("#chatbox_" + chatboxtitle + " .chatboxtextarea").addClass('chatboxtextareaselected');
-        });
-
-        $("#chatbox_" + chatboxtitle).click(function() {
-            if ($('#chatbox_' + chatboxtitle + ' .chatboxcontent').css('display') != 'none') {
-                $("#chatbox_" + chatboxtitle + " .chatboxtextarea").focus();
-            }
-        });
-
-        $("#chatbox_" + chatboxtitle).show();
-    }
-
-    function addToMessage(chatboxtitle, username, message) {
-        $("#chatbox_" + chatboxtitle + " .chatboxcontent").append('<div class="chatboxmessage"><span class="chatboxmessagefrom">' + username + ':&nbsp;&nbsp;</span><span class="chatboxmessagecontent">' + message + '</span></div>');
-    }
-
-    function checkChatBoxInputKey(event, chatboxtextarea, chatboxtitle) {
-
-        if (event.keyCode == 13 && event.shiftKey == 0) {
-            message = $(chatboxtextarea).val();
-            message = message.replace(/^\s+|\s+$/g, "");
-
-            $(chatboxtextarea).val('');
-            $(chatboxtextarea).focus();
-            $(chatboxtextarea).css('height', '44px');
-            if (message != '') {
-                message = message.replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/\"/g, "&quot;");
-                hub.server.sendMessage(username + ":::!" + message);
-                $("#chatbox_" + chatboxtitle + " .chatboxcontent").append('<div class="chatboxmessage"><span class="chatboxmessagefrom">' + username + ':&nbsp;&nbsp;</span><span class="chatboxmessagecontent">' + message + '</span></div>');
-                $("#chatbox_" + chatboxtitle + " .chatboxcontent").scrollTop($("#chatbox_" + chatboxtitle + " .chatboxcontent")[0].scrollHeight);
-            }
-
-            return false;
-        }
-
-        var adjustedHeight = chatboxtextarea.clientHeight;
-        var maxHeight = 94;
-
-        if (maxHeight > adjustedHeight) {
-            adjustedHeight = Math.max(chatboxtextarea.scrollHeight, adjustedHeight);
-            if (maxHeight)
-                adjustedHeight = Math.min(maxHeight, adjustedHeight);
-            if (adjustedHeight > chatboxtextarea.clientHeight)
-                $(chatboxtextarea).css('height', adjustedHeight + 8 + 'px');
-        } else {
-            $(chatboxtextarea).css('overflow', 'auto');
-        }
-    }
-
-    function closeChatBox(chatboxtitle) {
-        $('#chatbox_' + chatboxtitle).css('display', 'none');
-        var value2 = $("#chatName").val();
-        hub.server.leaveRoom(value2);
-        restructureChatBoxes();
-        var index = openedConversations.indexOf(value2);
-        if (index > -1) {
-            Array.splice(index, 1);
-        }
-    }
 });
+
+
+function chatWith(chatuser) {
+    openedConversations.push(chatuser);
+    createChatBox(chatuser);
+    $("#chatbox_" + chatuser + " .chatboxtextarea").focus();
+}
+
+function createChatBox(chatboxtitle, minimizeChatBox) {
+    if ($("#chatbox_" + chatboxtitle).length > 0) {
+        if ($("#chatbox_" + chatboxtitle).css('display') == 'none') {
+            $("#chatbox_" + chatboxtitle).css('display', 'block');
+            restructureChatBoxes();
+        }
+        $("#chatbox_" + chatboxtitle + " .chatboxtextarea").focus();
+        return;
+    }
+
+    $(" <div />").attr("id", "chatbox_" + chatboxtitle)
+        .addClass("chatbox")
+        .html('<div class="chatboxhead"><div class="chatboxtitle">' + chatboxtitle + '</div><div class="chatboxoptions"><a href="javascript:void(0)" onclick="javascript:toggleChatBoxGrowth(\'' + chatboxtitle + '\')">-</a> <a href="javascript:void(0)" onclick="javascript:closeChatBox(\'' + chatboxtitle + '\')">X</a></div><br clear="all"/></div><div class="chatboxcontent"></div><div class="chatboxinput"><textarea class="chatboxtextarea" onkeydown="javascript:return checkChatBoxInputKey(event,this,\'' + chatboxtitle + '\');"></textarea></div>')
+        .appendTo($("body"));
+
+    $("#chatbox_" + chatboxtitle).css('bottom', '0px');
+
+    chatBoxeslength = 0;
+
+    for (x in chatBoxes) {
+        if ($("#chatbox_" + chatBoxes[x]).css('display') != 'none') {
+            chatBoxeslength++;
+        }
+    }
+
+    if (chatBoxeslength == 0) {
+        $("#chatbox_" + chatboxtitle).css('right', '20px');
+    } else {
+        width = (chatBoxeslength) * (225 + 7) + 20;
+        $("#chatbox_" + chatboxtitle).css('right', width + 'px');
+    }
+
+    chatBoxes.push(chatboxtitle);
+
+    if (minimizeChatBox == 1) {
+        minimizedChatBoxes = new Array();
+
+        if ($.cookie('chatbox_minimized')) {
+            minimizedChatBoxes = $.cookie('chatbox_minimized').split(/\|/);
+        }
+        minimize = 0;
+        for (j = 0; j < minimizedChatBoxes.length; j++) {
+            if (minimizedChatBoxes[j] == chatboxtitle) {
+                minimize = 1;
+            }
+        }
+
+        if (minimize == 1) {
+            $('#chatbox_' + chatboxtitle + ' .chatboxcontent').css('display', 'none');
+            $('#chatbox_' + chatboxtitle + ' .chatboxinput').css('display', 'none');
+        }
+    }
+
+    chatboxFocus[chatboxtitle] = false;
+
+    $("#chatbox_" + chatboxtitle + " .chatboxtextarea").blur(function() {
+        chatboxFocus[chatboxtitle] = false;
+        $("#chatbox_" + chatboxtitle + " .chatboxtextarea").removeClass('chatboxtextareaselected');
+    }).focus(function() {
+        chatboxFocus[chatboxtitle] = true;
+        newMessages[chatboxtitle] = false;
+        $('#chatbox_' + chatboxtitle + ' .chatboxhead').removeClass('chatboxblink');
+        $("#chatbox_" + chatboxtitle + " .chatboxtextarea").addClass('chatboxtextareaselected');
+    });
+
+    $("#chatbox_" + chatboxtitle).click(function() {
+        if ($('#chatbox_' + chatboxtitle + ' .chatboxcontent').css('display') != 'none') {
+            $("#chatbox_" + chatboxtitle + " .chatboxtextarea").focus();
+        }
+    });
+
+    $("#chatbox_" + chatboxtitle).show();
+}
+
+function addToMessage(chatboxtitle, username, message) {
+    $("#chatbox_" + chatboxtitle + " .chatboxcontent").append('<div class="chatboxmessage"><span class="chatboxmessagefrom">' + username + ':&nbsp;&nbsp;</span><span class="chatboxmessagecontent">' + message + '</span></div>');
+}
+
+function checkChatBoxInputKey(event, chatboxtextarea, chatboxtitle) {
+
+    if (event.keyCode == 13 && event.shiftKey == 0) {
+        message = $(chatboxtextarea).val();
+        message = message.replace(/^\s+|\s+$/g, "");
+
+        $(chatboxtextarea).val('');
+        $(chatboxtextarea).focus();
+        $(chatboxtextarea).css('height', '44px');
+        if (message != '') {
+            message = message.replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/\"/g, "&quot;");
+            hub.server.sendMessage(username + ":::!" + message);
+            $("#chatbox_" + chatboxtitle + " .chatboxcontent").append('<div class="chatboxmessage"><span class="chatboxmessagefrom">' + username + ':&nbsp;&nbsp;</span><span class="chatboxmessagecontent">' + message + '</span></div>');
+            $("#chatbox_" + chatboxtitle + " .chatboxcontent").scrollTop($("#chatbox_" + chatboxtitle + " .chatboxcontent")[0].scrollHeight);
+        }
+
+        return false;
+    }
+
+    var adjustedHeight = chatboxtextarea.clientHeight;
+    var maxHeight = 94;
+
+    if (maxHeight > adjustedHeight) {
+        adjustedHeight = Math.max(chatboxtextarea.scrollHeight, adjustedHeight);
+        if (maxHeight)
+            adjustedHeight = Math.min(maxHeight, adjustedHeight);
+        if (adjustedHeight > chatboxtextarea.clientHeight)
+            $(chatboxtextarea).css('height', adjustedHeight + 8 + 'px');
+    } else {
+        $(chatboxtextarea).css('overflow', 'auto');
+    }
+}
+
+function closeChatBox(chatboxtitle) {
+    $('#chatbox_' + chatboxtitle).css('display', 'none');
+    var value2 = $("#chatName").val();
+    hub.server.leaveRoom(value2);
+    restructureChatBoxes();
+    var index = openedConversations.indexOf(value2);
+    if (index > -1) {
+        Array.splice(index, 1);
+    }
+}
+

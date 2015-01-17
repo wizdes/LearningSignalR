faacec27fd8e701f36df289bc93e5d1532a068a4
Yi Li <yl2326@cornell.edu>
2015-01-17 01:33:06 -0800

    added the cards on the screen

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
 roadmap:
-1) when getting the cards, list them correctly 
-- UI work
-- your cards are at the bottom, then back of the cards for all the others
+1) when getting the cards, list them correctly (done)
+- UI work (done)
+- your cards are at the bottom, then back of the cards for all the others (working on that)
   3
 2   4
   1
 
 3) clicking on a card brings it to the center (located correctly)
+3.5) clicking on a card brings it to the center *for everyone*
 4) logic needs to be added: only click on your own cards
 5) logic for the first phase (pick up/choose trump)
 6) logic needs to be added: only click on your own cards on your turn
--- SignalRBasic/SignalRBasic/Scripts/CreateGame.js
+++ SignalRBasic/SignalRBasic/Scripts/CreateGame.js
@@ -1,6 +1,38 @@
 ﻿var img;
 var imgArray;
 
+function setupOtherUsers() {
+    cardBackImage = new Image();
+    cardBackImage.src = "Resources/cardBack.jpg";
+
+    for (i = 0; i < 5; i++) {
+        bmp = new createjs.Bitmap(cardBackImage);
+        // this assumes hand count of 5
+        bmp.x = gameStage.canvas.width / 4 + gameStage.canvas.width / 10 * i;
+        bmp.y = 140;
+        bmp.scaleX = bmp.scaleY = 0.32;
+        gameStage.addChild(bmp);
+    }
+
+    for (i = 0; i < 5; i++) {
+        bmp = new createjs.Bitmap(cardBackImage);
+        // this assumes hand count of 5
+        bmp.x = 50;
+        bmp.y = gameStage.canvas.height / 6 + 2 * gameStage.canvas.height / 15 * i;
+        bmp.scaleX = bmp.scaleY = 0.32;
+        gameStage.addChild(bmp);
+    }
+
+    for (i = 0; i < 5; i++) {
+        bmp = new createjs.Bitmap(cardBackImage);
+        // this assumes hand count of 5
+        bmp.x = gameStage.canvas.width - 50 - 130;
+        bmp.y = gameStage.canvas.height / 6 + 2* gameStage.canvas.height / 15 * i;
+        bmp.scaleX = bmp.scaleY = 0.32;
+        gameStage.addChild(bmp);
+    }
+}
+
 function initGamePage(cardList) {
     if (window.top != window) {
         document.getElementById("header").style.display = "none";
@@ -27,6 +59,8 @@ function initGamePage(cardList) {
 
     handiter = 0;
 
+    setupOtherUsers();
+
     for (i = 0; i < 52; i++) {
         var shouldShow = false;
         for (j = 0; j < cardList.length; j++) {
@@ -40,9 +74,11 @@ function initGamePage(cardList) {
         }
 
         bmp = new createjs.Bitmap(imgArray[i]);
-        bmp.x = 5 + gameStage.canvas.width / 13 * (handiter % 13);
-        bmp.y = 5 + gameStage.canvas.height / 10 * 9;
+        // this assumes hand count of 5
+        bmp.x = gameStage.canvas.width/4 + gameStage.canvas.width /10 * handiter;
+        bmp.y = gameStage.canvas.height - 175;
         bmp.scaleX = bmp.scaleY = 0.18;
+        //130 - 70  
 
         handiter = handiter + 1;
 
